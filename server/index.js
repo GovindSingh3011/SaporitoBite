502c71a372ba868faffe46b74f79dc868d6cdb33
GovindSingh3011 <govindrawat3011@gmail.com>
2025-05-27 20:26:54 +0530

    Add morgan middleware for logging

--- server/index.js
+++ server/index.js
@@ -1,17 +1,19 @@
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
 const PORT = 3000;
 
-app.get('/', (req, res)=>{
+app.use(morgan('dev'));
+
+app.get('/', (req, res) => {
     res.status(200);
-    res.send("Welcome to root URL of SpoonSage Server");
+    res.send(`<title>SpioonSage Server</title> Welcome to root URL of SpoonSage Server`);
 });
 
-app.listen(PORT, (error) =>{
-    if(!error)
+app.listen(PORT, (error) => {
+    if (!error)
         console.log(`Server running on http://localhost:${PORT}`);
-    else 
+    else
         console.log("Error occurred, server can't start", error);
-    }
-);
+});

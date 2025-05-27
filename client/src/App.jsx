d60685fc76902612cd7f3003c61dbfd9d7abcc6d
GovindSingh3011 <govindrawat3011@gmail.com>
2025-05-27 16:57:34 +0530

    Add initial setup for React application with Tailwind CSS

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import SpoonSageLogo from './assets/SpoonSage.svg'
+
+function App() {
+  return (
+    <>
+        <a href="" target="_blank">
+          <img src={SpoonSageLogo} className="logo react" alt="React logo" />
+        </a>
+        <h1 className='underline'>SpoonSage</h1>
+    </>
+  )
+}
+
+export default App

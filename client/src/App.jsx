3730c91290d8ee92662f88efa6e2aa0e181867b8
GovindSingh3011 <govindrawat3011@gmail.com>
2025-08-07 11:47:19 +0530

    refactor: reorganize App component structure and improve main entry point

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,22 @@
-import Footer from "./components/Footer"
-import Navbar from "./components/Navbar"
+import { Routes, Route } from 'react-router-dom';
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import Recipes from "./pages/Recipes";
+import Home from "./pages/Home";
+import RecipeTypePage from './pages/RecipeTypePage';
 
 function App() {
   return (
-    <>
-        <Navbar />
-        
-        <Footer />
-    </>
-  )
+    <div className="font-serif">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/recipes" element={<Recipes />} />
+        <Route path="/recipes/type/:type" element={<RecipeTypePage />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
 }
 
-export default App
+export default App;
